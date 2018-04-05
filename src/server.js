d179328e8b8791323c0258f41bbe04949cdcd42f
Giulia <teggi.giulia@gmail.com>
2018-04-05 14:27:29 +0100

    add server function

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const http = require('http'); 
+const router = require('./router'); 
+
+require('env2')('./config.env');
+
+const host = process.env.HOST || 'localhost'; 
+const port = process.env.PORT || 4000; 
+
+const server = http.createServer(router); 
+
+server.listen(port, () => {
+    console.log('Server listening on port:', port); 
+})
