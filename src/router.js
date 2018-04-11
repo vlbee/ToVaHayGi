671b915434c931d9eb5167b0c1c94e865cf1364c
Giulia <teggi.giulia@gmail.com>
2018-04-11 16:21:47 +0100

    fix router

--- src/router.js
+++ src/router.js
@@ -4,23 +4,6 @@ const router = (req, res) => {
     const endpoint = req.url; 
     
     if (endpoint === '/') {
-<<<<<<< HEAD
-        staticHandler('public/auth.html', res) 
-    }
-    else if (endpoint === '/login'){
-        (function loginHandler(req, res){
-            let body = ''; 
-            req.on('data', function(chunk){
-                body += chunk; 
-            })
-            req.on('end', function(){
-                console.log("Body:", body); 
-            })
-        })(req, res); 
-    } 
-    else if (endpoint.indexOf('public') !== -1) {
-        staticHandler(endpoint, res); 
-=======
         //function to check JWT to see if user is already logged in
         //redirects either to login page or directly to index.html
         if(true){ //if logged in
@@ -42,7 +25,6 @@ const router = (req, res) => {
         //this comes in from auth.html to sign up a user
         //this will redirect to profile.html (editable state)
 
->>>>>>> b312a0db1e6883a932a3448b9522911a69f3f3a3
     }
     //this will be redundant when '/' directs to userlist
     else if (endpoint === '/userlist') {
