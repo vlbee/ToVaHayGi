ae2a09540640a4dab4cab4f3edc4bb5fae2b526d
HA <haydnba@gmail.com>
2018-04-12 20:16:07 +0100

    update populate user profile function - no hard code

--- public/profile.js
+++ public/profile.js
@@ -1,37 +1,28 @@
-var updateForm = document.getElementById("updateProfile"); 
-// var formFields = updateForm.childNodes; 
+var updateForm = document.getElementById("updateProfile");
+var formFields = updateForm.childNodes;
 
-var username = document.getElementById("username"); 
-var firstname = document.getElementById("firstname"); 
-var lastname = document.getElementById("lastname"); 
-var email = document.getElementById("email"); 
-var cohort = document.getElementById("cohort"); 
-var city = document.getElementById("city"); 
-var aboutme = document.getElementById("aboutme"); 
+var username = document.getElementById("username");
+var firstname = document.getElementById("firstname");
+var lastname = document.getElementById("lastname");
+var email = document.getElementById("email");
+var cohort = document.getElementById("cohort");
+var city = document.getElementById("city");
+var aboutme = document.getElementById("aboutme");
 
-function populateUserProfile(userData){ 
-    var parsedData = userData[0]; 
-    console.log("Parsed data", parsedData);
+function populateUserProfile(userData) {
+  var parsedData = userData[0];
+  console.log("Parsed data", parsedData);
 
-    // formFields.forEach(function(item){
-       
-    //   formFields.item.value = parsedData.item
-    // }); 
-    
-username.value = parsedData.handle; 
-firstname.value = parsedData.firstname; 
-email.value = parsedData.email; 
+  formFields.forEach(function(item) {
+    if (item.name) {
+        console.log(item.name);
+        item.value = parsedData[item.name];
+    }
+  });
 
 }
 
-  
-
-
-
 // IFFE on load
-(function () {
-
-    clientRequest('GET', '/profile-data', null, populateUserProfile);
-  
-  })(); 
-
+(function() {
+  clientRequest("GET", "/profile-data", null, populateUserProfile);
+})();
